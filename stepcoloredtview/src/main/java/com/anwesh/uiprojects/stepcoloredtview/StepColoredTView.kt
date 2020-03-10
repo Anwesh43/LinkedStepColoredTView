231ab2611376851f15c516b59d30b820e4535f72
Anwesh43 <anweshthecool0@gmail.com>
2020-03-10 10:20:36 +0530

    adding constants and libraries

--- stepcoloredtview/src/main/java/com/anwesh/uiprojects/stepcoloredtview/StepColoredTView.kt
+++ stepcoloredtview/src/main/java/com/anwesh/uiprojects/stepcoloredtview/StepColoredTView.kt
@@ -4,4 +4,17 @@ package com.anwesh.uiprojects.stepcoloredtview
  * Created by anweshmishra on 10/03/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
 
+val colors : Array<String> = arrayOf("#3F51B5", "#1A237E", "#f44336", "#4CAF50", "#BF360C")
+val lines : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20

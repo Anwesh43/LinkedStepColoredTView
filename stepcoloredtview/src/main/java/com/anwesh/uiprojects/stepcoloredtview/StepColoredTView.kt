ed0c7c1e624a5e3d530003f30cc0b79ddde3919d
Anwesh43 <anweshthecool0@gmail.com>
2020-03-10 10:54:16 +0530

    using renderer in view

--- stepcoloredtview/src/main/java/com/anwesh/uiprojects/stepcoloredtview/StepColoredTView.kt
+++ stepcoloredtview/src/main/java/com/anwesh/uiprojects/stepcoloredtview/StepColoredTView.kt
@@ -58,14 +58,16 @@ fun Canvas.drawSCTNode(i : Int, scale : Float, paint : Paint) {
 
 class StepColoredTView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -187,8 +189,9 @@ class StepColoredTView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val sct : StepColoredT = StepColoredT(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun draw(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             sct.draw(canvas, paint)
             animator.animate {
